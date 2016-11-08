a3384e97aacf697bd9e9ed860ab84f92de07bbee
Erik <erik.ring.walters@gmail.com>
2016-11-08 15:38:17 -0800

    got iterative and recursive fibonacci sequence functions working

--- Lecture19Recursion/Lecture19Recursion/Source.cpp
+++ Lecture19Recursion/Lecture19Recursion/Source.cpp
@@ -4,12 +4,55 @@ int recursiveFactorial(int n)
 {
 	if (n == 1)//base case
 	{
-		return n;
+		return n;//or "return 1;"
 	}
 	else//recursive case
 	{
 		return n * recursiveFactorial(n - 1);
 	}
+}
+
+double recursiveFibonacci(double f)
+{
+	if (f == 0)//base case 1
+	{
+		return 0;
+	}
+	else if (f == 1)//base case 2
+	{
+		return 1;
+	}
+	else
+	{
+		return recursiveFibonacci(f - 2) + recursiveFibonacci(f - 1);
+	}
+}
+
+int iterativeFibonacci(int n)//not working
+{
+	int f = 0;
+	int a, b, c;
+	if (n == 0)
+	{
+		return 0;
+	}
+	if (n == 1)
+	{
+		return 1;
+	}
+	else
+
+	
+	a = 0;
+	b = 1;
+	while (f<n - 1)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		f++;
+	}
+	return c;
 }
 
 void main()
@@ -20,4 +63,9 @@ void main()
 	cin >> input;
 	answer = recursiveFactorial(input);
 	cout << input << "! = " << answer << endl;
+	double input2;
+	cout << "Testing Fibonacci functions. Please enter a number: ";
+	cin >> input2;
+	cout << "Recursive: " << recursiveFibonacci(input2) << endl;
+	cout << "Iterative: " << iterativeFibonacci(input2) << endl;
 }
